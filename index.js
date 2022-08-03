a20dc1ad8582058cc4ab3449ad7d49bf959d2ad3
Daniel Isaac <dillydot22@gmail.com>
2022-08-03 22:59:25 +0100

    First

---
+++ index.js
@@ -0,0 +1,14 @@
+let clear = document.querySelector(".clear");
+let pN = document.querySelector(".pN");
+let percent = document.querySelector(".percent");
+let divide= document.querySelector(".division");
+let multiply = document.querySelector(".multiply");
+let subtract = document.querySelector(".subtract");
+let add = document.querySelector(".add");
+let dot = document.querySelector(".dot");
+let backspace = document.querySelector(".backspace");
+let equalTo = document.querySelector(".equalsto");
+
+clear.addEventListener('click', ()=>{
+  alert("I'm working, clear stuff")
+})
