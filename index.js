5385c41745818a54fb0d8c47eff7d38f5ed4b6ac
Daniel Isaac <dillydot22@gmail.com>
2022-08-07 21:27:01 +0100

    calc buggy

--- index.js
+++ index.js
@@ -1,63 +1,95 @@
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
     let allNumbers = document.querySelectorAll(".number");
-    let allOperators = document.querySelectorAll(".operator")
-    let calcDisplay = document.querySelector(".final-result")
-    let prevCalc = document.querySelector(".calc-display")
-    let finalVal = 0;
-    let prevResult = 0;
-    let calculationArr = [];
-    // clear.addEventListener('click', ()=> {
-    //   alert("I'm working, clear stuff")
-    //   console.log(allNum)
-    // })
+    let allOperators = document.querySelectorAll(".operator");
+    let calcDisplay = document.querySelector(".final-result");
+    let prevCalc = document.querySelector(".calc-display");
+    let decimal = document.querySelector(".decimal");
+
+    let operand1 = '';
+    let operand2 = '';
+    let operator = '';
+    let result = '';
+
+    const operators = {
+      '+': (a, b) => a + b,
+      '*': (a, b) => a * b,
+      '-': (a, b) => a - b,
+      '/': (a, b) => a / b,
+      '%': (a, b) => a % b,
+    };
 
     for (let i = 0; i < allNumbers.length; i++) {
       allNumbers[i].addEventListener('click', displayNum);
     }
+
     for (let i = 0; i < allOperators.length; i++){
       allOperators[i].addEventListener('click', checkOperators)
     }
 
-    function displayNum (val){
-      let value = val.target.innerHTML;
-      calculationArr.push(value);
-      calcDisplay.innerHTML = value;
-      console.log(calculationArr)
+    function displayNum (val) {
+
+      const value = val.target.innerHTML;
+      operator ? operand2 += value : operand1 += value;
+      calcDisplay.innerHTML = operator ? operand2 : operand1;
     }
 
-    function checkOperators(val){
-      let value = val.target.innerHTML;
-      switch(value) {
-        case "/":
-        case "-":
-        case "*":
-        case "+":
-        case "%":
-          calculationArr.push(value);
-          break;
-        case "=":
-          finalVal = eval(calculationArr.join(''));
-          calcDisplay.innerHTML = finalVal;
-          prevCalc.innerHTML = calculationArr.join('');
+    function resetOperands() {
+      operand1 = '';
+      operand2 = '';
+      operator = '';
+    }
 
-          // prevCalc = calculationArr;
-          // finalVal = eval(calculationArr)
-          // calcDisplay.innerHTML = finalVal;
-          console.log(finalVal)
-          break;
-        case "C":
-          calculationArr = [];
+    function checkOperators(val) {
+      let value = val.target.innerHTML;
+
+      if (value === 'C') {
+        resetOperands();
+        calcDisplay.innerHTML = '0';
+        prevCalc.innerHTML = '';
+        return;
+      }
+
+      if (result && !operand1) {
+        operand1 = result;
+      }
+
+      // If there are no operands yet, prevent the use of an operator
+      // Prevent using `=` as the first operator
+      if (!operand1 || (!operator && value === '=')) return;
+
+      if (!operand2) {
+        operator = value;
+        return;
+      }
+
+      if (!operators[operator]) {
+        calcDisplay.innerHTML = 'Err!';
+        resetOperands();
+        return;
+      };
+
+      // Calculate
+      result = `${operators[operator](parseFloat(operand1), parseFloat(operand2))}`;
+      // result = Number(result.toFixed(1));
+      console.log(result)
+      prevCalc.innerHTML = `${operand1} ${operator} ${operand2}`;
+      resetOperands();
+      calcDisplay.innerHTML = result;
+
+      if (value !== '=') {
+        operand1 = result;
+        operator = value;
       }
     }
-
-
-
-
-
-
-
-
-
+    decimal.onclick = () => {
+      if(!operand1.includes(".")){
+        operand1+=".";
+      }else{
+        operand2+=".";
+      }
+      calcDisplay.innerHTML = operand1;
+      calcDisplay.innerHTML = operand2;
+    }
   })
 })();
